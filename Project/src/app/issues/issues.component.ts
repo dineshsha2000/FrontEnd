92fda059e10ac5249a625dafa198bbbb3a2fd9cc
DINESH SHA A B <dineshshababu@gmail.com>
2019-06-15 19:54:33 +0530

    dropbox created

--- Project/src/app/issues/issues.component.ts
+++ Project/src/app/issues/issues.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./issues.component.css']
 })
 export class IssuesComponent implements OnInit {
-  
+  department=['sprint 11.2','sprint 10','sprint 9.2','sprint 9.1'];
+  member=['ajay','ajaysiva','bharath','bharani','dinesh','fero'];
+
   constructor() { }
 
   ngOnInit() {
