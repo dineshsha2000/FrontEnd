757290b1b57bccf86b8e53a4bf5e17521f07cd54
DINESH SHA A B <dineshshababu@gmail.com>
2019-06-16 11:09:32 +0530

    erorr removed

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -10,10 +10,8 @@ import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { CreateIssueComponent } from './create-issue/create-issue.component';
 import { LoginComponent } from './login/login.component';
 import { IssuesComponent } from './issues/issues.component';
-<<<<<<< HEAD
+
 import { TeamissueComponent } from './teamissue/teamissue.component';
-=======
->>>>>>> 119f12d613ed6517b650fe1837d2f38bc13557aa
 
 @NgModule({
   declarations: [
@@ -23,13 +21,12 @@ import { TeamissueComponent } from './teamissue/teamissue.component';
     ProfileComponent,
     CreateIssueComponent,
     LoginComponent,
-<<<<<<< HEAD
     IssuesComponent,
-    TeamissueComponent
-=======
-    IssuesComponent
+    TeamissueComponent,
 
->>>>>>> 119f12d613ed6517b650fe1837d2f38bc13557aa
+    
+
+
   ],
   imports: [
     BrowserModule,
